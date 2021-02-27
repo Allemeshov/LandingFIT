653fca5c6953ffb80cb4911895b14edef0e4491a
Lemesh-Sashka <51176460+Lemesh-Sashka@users.noreply.github.com>
2021-02-27 23:08:27 +0300

    opening

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-// tl.to(".text", { y: "0%", duration: 1.5 });
 tl.to(".text-first", { y: "0%", duration: 1 });
 tl.to(".text-second", { y: "0%", duration: 1 });
 tl.to(".hide-first", { x: "-550%", duration: 0.75 });
 tl.to(".hide-second", { x: "550%", duration: 0.75 }, "-=0.75");
-// tl.to(".slider", { y: "-100%", duration: 2.25, delay: 1 });
-// tl.to(".intro", { y: "-100%", duration: 1.5 });
 tl.fromTo(".intro", { opacity: 1 }, { opacity: 0, duration: 1.5 }, "-=0.6");
-// tl.to(".intro", { y: "-100%", duration: 1 }, "-=0.5");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 2 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=0.5");
+tl.fromTo(".landing", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=2");
+tl.to(".intro", { y: "-100%" });
